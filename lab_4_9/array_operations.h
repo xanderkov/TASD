7f295b9c016392a5bef47bcb328cd64fc24d1351
Kovel Alexander <xanderkovel@gmail.com>
2021-10-18 13:57:35 +0300

    Ade timer

--- lab_4_9/array_operations.h
+++ lab_4_9/array_operations.h
@@ -1,1 +1,2 @@
 void start_array_menu();
+void infix_to_postfix(char infix[], char postfix[]);
--- lab_4_9/time.c
+++ lab_4_9/time.c
@@ -5,9 +5,31 @@
 
 void compare_time()
 {
-    stack arr;
+    
+    int64_t time;
+    struct timeval tv_start, tv_stop;
+    char str[N], post[N];
     for (int i = 0; i < 50; i++)
     {
-        push(&arr, c);
+        str[i * 2] = '1';
+        str[i * 2 + 1] = '+';
     }
+    time = 0;
+    for (int i = 0; i < N_TIME; i++)
+    {
+        gettimeofday(&tv_start, NULL);
+        infix_to_postfix(str, post);
+        gettimeofday(&tv_stop, NULL);
+        time += (tv_stop.tv_sec - tv_start.tv_sec) * 1000000LL + (tv_stop.tv_usec - tv_start.tv_usec);
+    }
+    printf("Время использование массивом = %lf mcs\n", (float)time / N_TIME);
+    time = 0;
+    for (int i = 0; i < N_TIME; i++)
+    {
+        gettimeofday(&tv_start, NULL);
+        infix_to_postfix_list(str, post);
+        gettimeofday(&tv_stop, NULL);
+        time += (tv_stop.tv_sec - tv_start.tv_sec) * 1000000LL + (tv_stop.tv_usec - tv_start.tv_usec);
+    }
+    printf("Время использование спском = %lf mcs\n", (float)time / N_TIME);
 }
